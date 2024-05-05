937198a478b861fdfc7d04f11613ea58bababd19
jutee7 <n9687582731@gmail.com>
2024-05-06 00:32:12 +0300

    forms finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Data from './components/Data';
+import Form from './components/Form';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Data/>
+      <Form/>
     </>
   )
 }
